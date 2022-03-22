2a08145ced70e046ec8eccc1f79b1c3b6eddc170
bradley <bradleybkempton@gmail.com>
2022-03-21 20:38:28 -0700

    added base structure for program

--- main.js
+++ main.js
@@ -1,1 +1,66 @@
+//Header
+const head = document.createElement("h1");
+head.textContent = "TIC TAC TOE";
+document.body.appendChild(head);
+
+//Container
+const container = document.createElement("div");
+container.classList.add("container");
+document.body.appendChild(container);
+
+//Create grid
+createGrid();
+
+//Create grid function
+function createGrid() {
+    for (let i = 0; i < 9; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row-div");
+        row.classList.add("write");
+        container.appendChild(row); 
+    }
+    markGrid();
+}
+
+//On Clock Play
+function markGrid() {
+    document.querySelectorAll(".write").forEach(item => {
+        item.addEventListener("click", event => {
+            //item.style.cssText = "background-color: black;"
+            item.textContent = "X";
+        })
+    });
+}
+
+//Reset background header text to blank slate
+const inst = document.createElement("h3");
+inst.textContent = "PRESS CTRL + R TO RESET";
+document.body.appendChild(inst);
+
+const reset = document;
+
+reset.addEventListener("keydown", (event) => {
+    if (event.ctrlKey && event.key === 'r') {
+        document.querySelectorAll(".write").forEach(item => {
+            item.style.cssText = "background-color: white;"
+        })
+    }
+});
+
+
+let Gameboard = {
+    gameboard: []
+}
+
+let playerOne = {
+
+}
+
+let playerTwo = {
+
+}
+
+let gameFlow = {
+
+}
 
