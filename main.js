51a8c0a48db61af10153819d815c58f5b0e6c2b7
bradley <bradleybkempton@gmail.com>
2022-04-01 16:19:47 -0700

    Added logic for player 1 and 2 along with corresponding signs, objects, and variables

--- main.js
+++ main.js
@@ -2,30 +2,138 @@
 (function createGrid() {
     //Header
     const head = document.createElement("h1");
-    head.textContent = "TIC TAC TOE";
+    head.textContent = "TIC - TAC - TOE";
     document.body.appendChild(head);
 
 
     //Gameboard
     let Gameboard = {
-        gameboard: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        gameboard: ["", "", "", "", "", "", "", "", ""]
     };
 
     let playerOne = {
-        sign: "Dog",
-        text: () => {footer.textContent = "Hi";}
+        sign: "X",
+        turn: true,
     };
 
     let playerTwo = {
-        sign: "O"
+        sign: "O",
+        turn: false,
     };
 
     let gameFlow = {
+        startStatus: true,
+
         start: () => {
-            
-        },
-        yo: () => {console.log("yo dog")}
-    };
+            footer.textContent = `N E W - G A M E :  P L A Y E R - O N E`;
+            gameFlow.turnTracker();
+
+            //DOM Marker Placement
+            document.querySelectorAll(".box-0").forEach(item => {
+                item.addEventListener("click", event => {
+                    Gameboard.gameboard[0] = gameFlow.playerSignProp;
+                    item.textContent = Gameboard.gameboard[0];
+                    gameFlow.switchPlayer();
+                    gameFlow.turnTracker();
+                })
+            });
+            document.querySelectorAll(".box-1").forEach(item => {
+                item.addEventListener("click", event => {
+                    Gameboard.gameboard[1] = gameFlow.playerSignProp;
+                    item.textContent = Gameboard.gameboard[1];
+                    gameFlow.switchPlayer();
+                    gameFlow.turnTracker();
+                })
+            });
+            document.querySelectorAll(".box-2").forEach(item => {
+                item.addEventListener("click", event => {
+                    Gameboard.gameboard[2] = gameFlow.playerSignProp;
+                    item.textContent = Gameboard.gameboard[2];
+                    gameFlow.switchPlayer();
+                    gameFlow.turnTracker();
+                })
+            });
+            document.querySelectorAll(".box-3").forEach(item => {
+                item.addEventListener("click", event => {
+                    Gameboard.gameboard[3] = gameFlow.playerSignProp;
+                    item.textContent = Gameboard.gameboard[3];
+                    gameFlow.switchPlayer();
+                    gameFlow.turnTracker();
+                })
+            });
+            document.querySelectorAll(".box-4").forEach(item => {
+                item.addEventListener("click", event => {
+                    Gameboard.gameboard[4] = gameFlow.playerSignProp;
+                    item.textContent = Gameboard.gameboard[4];
+                    gameFlow.switchPlayer();
+                    gameFlow.turnTracker();
+                })
+            });
+            document.querySelectorAll(".box-5").forEach(item => {
+                item.addEventListener("click", event => {
+                    Gameboard.gameboard[5] = gameFlow.playerSignProp;
+                    item.textContent = Gameboard.gameboard[5];
+                    gameFlow.switchPlayer();
+                    gameFlow.turnTracker();
+                })
+            });
+            document.querySelectorAll(".box-6").forEach(item => {
+                item.addEventListener("click", event => {
+                    Gameboard.gameboard[6] = gameFlow.playerSignProp;
+                    item.textContent = Gameboard.gameboard[6];
+                    gameFlow.switchPlayer();
+                    gameFlow.turnTracker();
+                })
+            });
+            document.querySelectorAll(".box-7").forEach(item => {
+                item.addEventListener("click", event => {
+                    Gameboard.gameboard[7] = gameFlow.playerSignProp;
+                    item.textContent = Gameboard.gameboard[7];
+                    gameFlow.switchPlayer();
+                    gameFlow.turnTracker();
+                })
+            });
+            document.querySelectorAll(".box-8").forEach(item => {
+                item.addEventListener("click", event => {
+                    Gameboard.gameboard[8] = gameFlow.playerSignProp;
+                    item.textContent = Gameboard.gameboard[8];
+                    gameFlow.switchPlayer();
+                    gameFlow.turnTracker();
+                })
+            });
+            document.querySelectorAll(".box-9").forEach(item => {
+                item.addEventListener("click", event => {
+                    Gameboard.gameboard[9] = gameFlow.playerSignProp;
+                    item.textContent = Gameboard.gameboard[9];
+                    gameFlow.switchPlayer();
+                    gameFlow.turnTracker();
+                })
+            });
+        }, // End gameFLow.start property
+
+        turnTracker: () => {
+            if (playerOne.turn === true) {
+                gameFlow.playerSignProp = playerOne.sign;
+            } else if (playerTwo.turn === true) {
+                gameFlow.playerSignProp = playerTwo.sign;
+            }
+
+        },  //end gameFlow.turnTracker
+
+        playerSignProp: "",
+
+        switchPlayer: () => {
+            if (playerOne.turn === true) {
+                playerOne.turn = false;
+                playerTwo.turn = true;
+                footer.textContent = "P L A Y E R - T W O";
+            } else if (playerTwo.turn === true) {
+                playerTwo.turn = false;
+                playerOne.turn = true;
+                footer.textContent = "P L A Y E R - O N E";
+            }
+        }
+    };//End gameFlow object
 
     //Container
     const container = document.createElement("div");
@@ -41,111 +149,41 @@
         container.appendChild(row);
     }
 
-    document.querySelectorAll(".box-0").forEach(item => {
-        item.addEventListener("click", event => {
-            Gameboard.gameboard[0] = playerOne.sign;
-            item.textContent = Gameboard.gameboard[0];
-        })
-    });
-    document.querySelectorAll(".box-1").forEach(item => {
-        item.addEventListener("click", event => {
-            Gameboard.gameboard[1] = "X";
-            item.textContent = Gameboard.gameboard[1];
-        })
-    });
-    document.querySelectorAll(".box-2").forEach(item => {
-        item.addEventListener("click", event => {
-            Gameboard.gameboard[2] = "X";
-            item.textContent = Gameboard.gameboard[2];
-        })
-    });
-    document.querySelectorAll(".box-3").forEach(item => {
-        item.addEventListener("click", event => {
-            Gameboard.gameboard[3] = "X";
-            item.textContent = Gameboard.gameboard[3];
-        })
-    });
-    document.querySelectorAll(".box-4").forEach(item => {
-        item.addEventListener("click", event => {
-            Gameboard.gameboard[4] = "X";
-            item.textContent = Gameboard.gameboard[4];
-        })
-    });
-    document.querySelectorAll(".box-5").forEach(item => {
-        item.addEventListener("click", event => {
-            Gameboard.gameboard[5] = "X";
-            item.textContent = Gameboard.gameboard[5];
-        })
-    });
-    document.querySelectorAll(".box-6").forEach(item => {
-        item.addEventListener("click", event => {
-            Gameboard.gameboard[6] = "X";
-            item.textContent = Gameboard.gameboard[6];
-        })
-    });
-    document.querySelectorAll(".box-7").forEach(item => {
-        item.addEventListener("click", event => {
-            Gameboard.gameboard[7] = "X";
-            item.textContent = Gameboard.gameboard[7];
-        })
-    });
-    document.querySelectorAll(".box-8").forEach(item => {
-        item.addEventListener("click", event => {
-            Gameboard.gameboard[8] = "X";
-            item.textContent = Gameboard.gameboard[8];
-        })
-    });
-    document.querySelectorAll(".box-9").forEach(item => {
-        item.addEventListener("click", event => {
-            Gameboard.gameboard[9] = "X";
-            item.textContent = Gameboard.gameboard[9];
-        })
-    });
-        
     //Click Container to start game
     document.querySelectorAll(".container").forEach(item => {
         item.addEventListener("click", event => {
-            gameFlow.yo;
-           
+            //Start Game on Container Click
+            if (gameFlow.startStatus === true) {
+                gameFlow.start();
+                gameFlow.startStatus = false;
+            }
         })
     });
 
     //Footer
     const footer = document.createElement("h3");
-            footer.textContent = "START GAME";
-            document.body.appendChild(footer);
-            console.log("Start")
+    footer.textContent = "CLICK - TO - START - GAME";
+    document.body.appendChild(footer);
+
+    //Reset background header text to blank slate
+    const inst = document.createElement("h4");
+    inst.textContent = "R E S E T - (CTRL + R)";
+    document.body.appendChild(inst);
+
+    const reset = document;
+
+    reset.addEventListener("keydown", (event) => {
+        if (event.ctrlKey && event.key === 'r') {
+            document.querySelectorAll(".write").forEach(item => {
+                //item.style.cssText = "background-color: white;"
+                Gameboard.gameboard = ["", "", "", "", "", "", "", "", ""]
+                gameFlow.start();
+                gameFlow.startStatus = false;
+            })
+        }
+    });
+
+
 })();
-
-
-
-
-// //On Clock Play
-// function markGrid() {
-//     document.querySelectorAll(".row-div").forEach(item => {
-//         item.addEventListener("click", event => {
-//             //item.style.cssText = "background-color: black;"
-//             item.textContent = "X";
-//         })
-//     });
-// }
-
-// //Reset background header text to blank slate
-// const inst = document.createElement("h3");
-// inst.textContent = "PRESS CTRL + R TO RESET";
-// document.body.appendChild(inst);
-
-// const reset = document;
-
-// reset.addEventListener("keydown", (event) => {
-//     if (event.ctrlKey && event.key === 'r') {
-//         document.querySelectorAll(".write").forEach(item => {
-//             item.style.cssText = "background-color: white;"
-//         })
-//     }
-// });
-
-
-
 
 
